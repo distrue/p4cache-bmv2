3e5e30221ca1b9af932403b53b1eca9c3c2d96d1
Y.Jung <gilsang9808@gmail.com>
2020-03-20 11:00:46 +0900

    grpc connection check script

--- src/controller_go/controller.go
+++ src/controller_go/controller.go
@@ -7,42 +7,34 @@ import (
 	"log"
 	"sync"
 
+	p4config "github.com/distrue/p4goruntime/p4/config/v1"
 	p4runtime "github.com/distrue/p4goruntime/p4/v1"
 	grpc "google.golang.org/grpc"
-	// p4config "github.com/distrue/p4gontroller/p4/config/v1";
-	// "google.golang.org/grpc";
 )
 
 const (
 	address = "localhost:9090"
 )
 
-/*
-import grpc
-from p4.v1 import p4runtime_pb2
-from p4.tmp import p4config_pb2
-*/
-
 const MSG_LOG_MAX_LEN = 1024
 
-var connections []*SwitchConnection
+func GetSwitchConnection(name string, address string, device_id uint64) p4runtime.P4RuntimeClient {
+	if name == "" {
+		name = "client"
+	}
+	if address == "" {
+		address = "localhost:9090"
+	}
+	if device_id == 0 {
+		device_id = 1
+	}
+	conn, err := grpc.Dial(address, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	fmt.Println("grpc connected", conn.GetState())
 
-// def ShutdownAllSwitchConnections():
-// for c in connections:
-//    c.shutdown()
-
-type SwitchConnection struct {
-	name      string
-	address   string
-	device_id uint64
-	p4info    string // temporary
-	channel   *grpc.ClientConn
-	// if proto_dump_file is not None:
-	// interceptor = GrpcRequestLogger(proto_dump_file)
-	// self.channel = grpc.intercept_channel(self.channel, interceptor)
-	client_stub p4runtime.P4RuntimeClient
-	// requests_stream - temporary disable
-	ctx context.Context
+	return p4runtime.NewP4RuntimeClient(conn)
 }
 
 func OpenStreamListener(stream p4runtime.P4Runtime_StreamChannelClient) sync.WaitGroup {
@@ -71,27 +63,42 @@ func OpenStreamListener(stream p4runtime.P4Runtime_StreamChannelClient) sync.Wai
 	return waitg
 }
 
-func NewSwitchConnection(name string, address string, device_id uint64) *SwitchConnection {
-	if name == "" {
-		name = "client"
+func SetMastership(stream p4runtime.P4Runtime_StreamChannelClient) {
+	req := p4runtime.StreamMessageRequest{
+		Update: &p4runtime.StreamMessageRequest_Arbitration{
+			Arbitration: &p4runtime.MasterArbitrationUpdate{
+				DeviceId: 1,
+				Role: &p4runtime.Role{
+					Id: 2,
+				},
+				ElectionId: &p4runtime.Uint128{High: 10000, Low: 9999},
+			},
+		},
 	}
-	if address == "" {
-		address = "localhost:9090"
-	}
-	if device_id == 0 {
-		device_id = 1
-	}
-	conn, err := grpc.Dial(address, grpc.WithInsecure())
+
+	err := stream.Send(&req)
 	if err != nil {
-		log.Fatalf("did not connect: %v", err)
+		fmt.Println("ERROR SENDING STREAM REQUEST:")
+		fmt.Println(err)
 	}
-	fmt.Println("grpc connected", conn.GetState())
+}
 
-	c := p4runtime.NewP4RuntimeClient(conn)
-	ctx := context.Background()
-	fmt.Println("client generated")
+func GetPipelineConfigs(client p4runtime.P4RuntimeClient) (*p4config.P4Info, error) {
+	getReq := &p4runtime.GetForwardingPipelineConfigRequest{
+		DeviceId:     1,
+		ResponseType: p4runtime.GetForwardingPipelineConfigRequest_ALL,
+	}
+	reply, err := client.GetForwardingPipelineConfig(context.Background(), getReq)
+	if err != nil {
+		fmt.Println(err)
+		return nil, nil
+	}
+	return reply.GetConfig().P4Info, nil
+}
 
-	stream, sErr := c.StreamChannel(context.Background())
+func main() {
+	client := GetSwitchConnection("client", "localhost:9090", 1)
+	stream, sErr := client.StreamChannel(context.Background())
 	if sErr != nil {
 		fmt.Println(sErr)
 		log.Fatalf("cannot open stream channel with the server")
@@ -99,37 +106,13 @@ func NewSwitchConnection(name string, address string, device_id uint64) *SwitchC
 
 	listener := OpenStreamListener(stream)
 
-	switchConnection := &SwitchConnection{
-		name:        name,
-		address:     address,
-		device_id:   device_id,
-		channel:     conn,
-		client_stub: c,
-		ctx:         ctx,
-	}
-	connections = append(connections, switchConnection)
-	listener.Wait()
-	return switchConnection
-}
-
-func main() {
-	swt := NewSwitchConnection("client", "localhost:9090", 1)
-	cfgreq := &p4runtime.GetForwardingPipelineConfigRequest{
-		DeviceId:     swt.device_id,
-		ResponseType: p4runtime.GetForwardingPipelineConfigRequest_ALL,
-	}
-	fmt.Println(cfgreq.String())
-	pipeline, err := swt.client_stub.GetForwardingPipelineConfig(context.Background(), cfgreq)
+	SetMastership(stream)
+	newConfig, err := GetPipelineConfigs(client)
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
-	fmt.Println("get forwarding pipeline config")
-	info := pipeline.Config.GetP4Info()
-	fmt.Println("get p4info")
-	tables := info.GetTables()
-	fmt.Println("get tables")
-	for _, it := range tables {
-		fmt.Printf(it.String())
-	}
+	fmt.Printf("%+v\n", newConfig)
+
 	fmt.Printf("p4controller")
+	listener.Wait()
 }
